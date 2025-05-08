729d248e8e9953af2d591016bdb09989dae20319
Panupong Mikada <panupong.mikada@gmail.com>
2025-05-08 23:27:08 +0700

    install package godotenv

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/lMikadal/go-backend_api_user
 
 go 1.24.2
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
 	fmt.Println("Hello World")
+
+	// load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+
+	fmt.Println("Port: " + port)
 }
